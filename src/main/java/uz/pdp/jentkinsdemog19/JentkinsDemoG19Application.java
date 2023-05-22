4fca39929eb13bd4effc82736cffa82a437da5a6
@3110mmm3110 <3110MMM3110@gmail.com>
2023-05-22 11:53:34 +0500

    aaa

---
+++ src/main/java/uz/pdp/jentkinsdemog19/JentkinsDemoG19Application.java
@@ -0,0 +1,13 @@
+package uz.pdp.jentkinsdemog19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JentkinsDemoG19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JentkinsDemoG19Application.class, args);
+	}
+
+}
---
+++ src/test/java/uz/pdp/jentkinsdemog19/JentkinsDemoG19ApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.jentkinsdemog19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JentkinsDemoG19ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
